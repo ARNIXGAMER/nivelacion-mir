fea51fd647923461a6069cf1554d821c8d7f5512
ARNIXGAMER <arnix676@gmail.com>
2022-11-25 20:22:42 -0300

    Imprime nombre del primer ingrediente en consola

--- ejercicio7/index.js
+++ ejercicio7/index.js
@@ -2,7 +2,6 @@
 let receta = {
 
 }
-
 //paso 2
 receta.nombre = "Sandwich"
 //paso 3
@@ -11,3 +10,5 @@ receta.ingredientes = []
 receta.ingredientes.push({nombre: "Pan", cantidad: 2})
 //Paso 5
 receta.ingredientes.push({nombre: "Queso", cantidad: 1})
+//Paso 6
+console.log(receta.ingredientes[0].nombre)
