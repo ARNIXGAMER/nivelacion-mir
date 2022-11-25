7d285107f11eb46eb1233b657df4544f89088f20
ARNIXGAMER <arnix676@gmail.com>
2022-11-25 20:27:33 -0300

    Imprime el total de las propiedades cantidad en consola

--- ejercicio7/index.js
+++ ejercicio7/index.js
@@ -12,3 +12,5 @@ receta.ingredientes.push({nombre: "Pan", cantidad: 2})
 receta.ingredientes.push({nombre: "Queso", cantidad: 1})
 //Paso 6
 console.log(receta.ingredientes[0].nombre)
+//paso 7
+console.log(receta.ingredientes[0].cantidad + receta.ingredientes[1].cantidad)
